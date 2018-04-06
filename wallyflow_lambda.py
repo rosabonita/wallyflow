2ed54b552b0a101c67e2a41866dd73e935755c07
Megan Rose Bryant <esa.aelfvine@gmail.com>
2018-04-06 19:07:18 -0400

    Update wallyflow_lambda.py
    Updated intents to retrieve values from AWS RDS and removed remaining AWS lambda template schema

--- wallyflow_lambda.py
+++ wallyflow_lambda.py
@@ -35,7 +35,7 @@ def build_response(session_attributes, speechlet_response):
 
 ########################################################
 def get_welcome_response():
-    session_attributes = {}
+    session_attributes={}
     card_title = "Welcome"
     speech_output = "Welcome to Wally Flow, mindfulness with Walabot and Alexa."\
                     "How can I help you?"
@@ -47,11 +47,65 @@ def get_welcome_response():
                           build_speechlet_response(card_title,speech_output,
                                                    reprompt_text,should_end_session))
 def get_mst_response():
-    session_attributes ={}
+    session_attributes={}
     card_title = "Maximum Still Time"
     sql = "SELECT MaxStillTime FROM wallyflowdb.mstTable ORDER BY initTime DESC LIMIT 1"
-    
-    
+    cursor.execute(sql)   
+    mst = cursor.fetchone()
+    speech_output = "Your maximum still time is set to {}.".format(mst[0])
+    reprompt_text = None
+    should_end_session = False
+    return build_response(session_attributes,
+                          build_speechlet_response(card_title,speech_output,
+                                                   reprompt_text,should_end_session))
+def get_mc_response():
+    session_attributes={}
+    card_title="Move Count"
+    sql = "SELECT MoveCount FROM wallyflowdb.mcTable ORDER BY initTime DESC LIMIT 1"
+    cursor.execute(sql)
+    mc = cursor.fetchone()
+    speech_output = "Your current movement count is {}.".format(mc[0])
+    reprompt_text = None
+    should_end_session = False
+    return build_response(session_attributes,
+                          build_speechlet_response(card_title,speech_output,
+                                                   reprompt_text,should_end_session))
+def get_stillTime_response():
+    session_attributes={}
+    card_title="Stillness Time"
+    sql = "SELECT StillTime FROM wallyflowdb.stillTable ORDER BY initTime DESC LIMIT 1"
+    cursor.execute(sql)
+    stillTime = cursor.fetchone()
+    speech_output = "Your current movement count is {}.".format(stillTime[0])
+    reprompt_text=None
+    should_end_session=False
+    return build_response(session_attributes,
+                          build_speechlet_response(card_title,speech_output,
+                                                   reprompt_text,should_end_session))
+def get_lmt_response():
+    session_attributes={}
+    card_title="Last Move Time"
+    sql = "SELECT LastMoveTime FROM wallyflowdb.lmtTable ORDER BY initTime DESC LIMIT 1"
+    cursor.execute(sql)
+    lmt = cursor.fetchone()
+    speech_output = "Your last move time was {}".format(lmt[0])
+    reprompt_text=None
+    should_end_session=False
+    return build_response(session_attributes,
+                          build_speechlet_response(card_title,speech_output,
+                                                   reprompt_text,should_end_session))
+def get_startTime_response():
+    session_attributes = {}
+    card_title="Start Time"
+    sql = "SELECT StartTime FROM wallyflowdb.stTable ORDER BY initTime DESC LIMIT 1"
+    cursor.execute(sql)
+    startTime = cursor.fetchone()
+    speech_output = "You started this mindfulness session at {}".format(startTime[0])
+    reprompt_text=None
+    should_end_session=False
+    return build_response(session_attributes,
+                          build_speechlet_response(card_title,speech_output,
+                                                   reprompt_text,should_end_session))    
 def handle_session_end_request():
     card_title = "Session Ended"
     speech_output = "Thank you for using Wally Flow, mindfulness with Walabot and Alexa."\
@@ -60,48 +114,6 @@ def handle_session_end_request():
     return build_response({}, build_speechlet_response(
         card_title, speech_output, None, should_end_session))
 
-def create_favorite_color_attribute(favorite_color):
-    return {"favoriteColor": favorite_color}
-
-def set_color_in_session(intent, session):
-    #Sets the maxStillTime in the session and prepares the speech to reply to the user."""
-    card_title = intent['name']
-    session_attributes = {}
-    should_end_session = False
-
-    if 'Color' in intent['slots']:
-        favorite_color = intent['slots']['Color']['value']
-        session_attributes = create_favorite_color_attributes(favorite_color)
-        speech_output = "I now know your favorite color is " + \
-                    favorite_color + \
-                    ". You can ask me your favorite color by saying, " \
-                    "what's my favorite color?"
-        reprompt_text = "You can ask me your favorite color by saying, " \
-                        "what's my favorite color?"
-    else:
-        speech_output = "I'm not sure what your favorite color is. " \
-                        "Please try again."
-        reprompt_text = "I'm not sure what your favorite color is. " \
-                        "You can tell me your favorite color by saying, "\
-                        "my favorite color is 'red'."
-        return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, shoud_end_session))
-
-def get_color_from_session(intent, session):
-    session_attributes = {}
-    reprompt_text = None
-
-    if session.get('attributes', {}) and "favoriteColor" in session.get('attributes', {}):
-        favorite_color = session['attributes']['favoriteColor']
-        speech_output = "Your favorite color is : " + favorite_color + \
-                        ". Goodbye."
-        should_end_session = True
-    else:
-        speech_output = "I'm not sure what your favorite color is. " \
-                        "You can say, my favorite color is 'red'."
-        should_end_session = False
-    return build_response(session_attributes, build_speechlet_response(
-        intent['name'], speech_output,reprompt_text,should_end_session))
 ###########################################################################
 #Events
 
@@ -125,17 +137,15 @@ def on_intent(intent_request, session):
     intent_name = intent_request['intent']['name']
 
     if intent_name == "MaxStillTimeIntent":
-        sql = "SELECT * FROM wallyflowdb.mstTable ORDER BY MaxStillTimeTS
-        
-        return #MaxStillTime
+        return get_mst_response()
     elif intent_name == "MoveCountIntent":
-        return #MoveCount
+        return get_mc_response()
     elif intent_name == "StillTimeIntent":
-        return #StillTime
+        return get_stillTime_response()
     elif intent_name == "LastMoveTimeIntent":
-        return #LastMoveTime"
+        return get_lmt_response()
     elif intent_name == "StartTimeIntent":
-        return #StartTimeIntent
+        return get_startTime_response()
     elif intent_name == "AMAZON.HelpIntent":
         return get_welcome_response()
     elif intent_name == "AMAZON.CANCELIntent" or intent_name == "AMAZON.StopIntent":
